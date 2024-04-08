7f57cdad530bd5a34f6a2d70dd0dc656f2ee910c
Isobel Daley <isobeldaley@DIT003876.local>
2024-04-08 20:31:53 +0100

    feat: add ebs iam policies

--- infra/ecs_main_arango.tf
+++ infra/ecs_main_arango.tf
@@ -300,6 +300,51 @@ resource "aws_iam_role_policy_attachment" "arango_ec2" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
 }
 
+resource "aws_iam_role_policy_attachment" "arango_ec2_ssm" {
+  role        = aws_iam_role.arango_ec2.name
+  policy_arn  = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+data "aws_iam_policy_document" "arango_ebs" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "ec2:AttachVolume",
+      "ec2:CreateVolume",
+      "ec2:CreateSnapshot",
+      "ec2:CreateTags",
+      "ec2:DeleteVolume",
+      "ec2:DeleteSnapshot",
+      "ec2:DescribeAvailabilityZones",
+      "ec2:DescribeInstances",
+      "ec2:DescribeVolumes",
+      "ec2:DescribeVolumeAttribute",
+      "ec2:DescribeVolumeStatus",
+      "ec2:DescribeSnapshots",
+      "ec2:CopySnapshot",
+      "ec2:DescribeSnapshotAttribute",
+      "ec2:DetachVolume",
+      "ec2:ModifySnapshotAttribute",
+      "ec2:ModifyVolumeAttribute",
+      "ec2:DescribeTags"
+    ]
+    resources = [
+      "*"
+    ]
+  }
+}
+
+resource "aws_iam_policy" "arango_ebs" {
+  name        = "arango-ebs"
+  description = "enable-mounting-of-ebs-volume"
+  policy      = data.aws_iam_policy_document.arango_ebs.json
+}
+
+resource "aws_iam_role_policy_attachment" "arango_ec2_ebs" {
+  role        = aws_iam_role.arango_ec2.name
+  policy_arn  = aws_iam_policy.arango_ebs.arn
+}
+
 resource "aws_iam_instance_profile" "arango_ec2" {
   name  = "${var.prefix}-arango-ec2"
   role  = aws_iam_role.arango_ec2.id
