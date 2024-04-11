4a3b62d1f58ff7ea3f70c23efaee6d29cec6c8f0
Isobel Daley <isobeldaley@DIT003876.local>
2024-04-11 13:53:57 +0100

    feat: mount EBS volume to EC2 instance

--- infra/ecs_main_arango_user_data.sh
+++ infra/ecs_main_arango_user_data.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 # install and start ecs agent 
 
+EC2_INSTANCE_ID=$(ec2-metadata --instance-id | sed 's/instance-id: //')
+aws ec2 attach-volume --volume-id vol-0496244e683c15b0a --instance-id ${EC2_INSTANCE_ID} --device /dev/xvdf --region eu-west-2
+# Follow symlinks to find the real device
+device=$(sudo readlink -f /dev/xvdf)
+  # Wait for the drive to be attached
+while [ ! -e $device ] ; do sleep 1 ; done
+# Format /dev/sdh if it does not contain a partition yet
+if [ "$(sudo file -b -s $device)" == "data" ]; then
+sudo mkfs -t ext4 $device
+fi
+# Mount the drive
+sudo mkdir -p /data
+sudo mount $device /data
+
 mkdir /etc/ecs/
 echo "ECS_CLUSTER=data-workspace-dev-a" >> /etc/ecs/ecs.config
-sudo yum update -y ecs-init
+sudo yum install -y ecs-init
 sudo systemctl restart ecs
-#echo "ECS_ENABLE_AWSLOGS_EXECUTIONROLE_OVERRIDE=true" >> /etc/ecs/ecs.config
-# install the REX-Ray Docker volume plugin
-#docker plugin install rexray/ebs "REXRAY_PREEMPT=true" "EBS_REGION=eu-west-2" --grant-all-permission
-# restart the ECS agent. This ensures the plugin is active and recognized once the agent starts.
-#sudo yum update -y ecs-init
-#sudo systemctl restart ecs
+
+
+
+
