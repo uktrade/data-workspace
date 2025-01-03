0d76c746027a7ba690cc25e121b1465156d5d915
agalamatis <argyris.galamatis@digital.trade.gov.uk>
2025-01-03 12:53:31 +0000

    expire theia images

--- infra/ecr.tf
+++ infra/ecr.tf
@@ -73,7 +73,7 @@ resource "aws_ecr_repository" "theia" {
 
 resource "aws_ecr_lifecycle_policy" "theia_expire_untagged_after_one_day" {
   repository = aws_ecr_repository.theia.name
-  policy     = data.aws_ecr_lifecycle_policy_document.expire_untagged_after_one_day.json
+  policy     = data.aws_ecr_lifecycle_policy_document.expire_non_master_non_latest_after_one_day.json
 }
 
 resource "aws_ecr_repository" "vscode" {
@@ -298,6 +298,52 @@ data "aws_ecr_lifecycle_policy_document" "visualisation_base_expire_old_after_on
     selection {
       tag_status       = "tagged"
       tag_pattern_list = ["rv4"]
+      count_type       = "sinceImagePushed"
+      count_unit       = "days"
+      count_number     = 365000
+    }
+  }
+  # ... and images that don't match python or rv4, but have a tag, are cached layers
+  rule {
+    priority = 3
+    selection {
+      tag_status       = "tagged"
+      tag_pattern_list = ["*"]
+      count_type       = "sinceImagePushed"
+      count_unit       = "days"
+      count_number     = 1
+    }
+  }
+  # ... and for when we end up with untagged images, expire them after 1 day as well
+  rule {
+    priority = 4
+    selection {
+      tag_status   = "untagged"
+      count_type   = "sinceImagePushed"
+      count_unit   = "days"
+      count_number = 1
+    }
+  }
+}
+
+data "aws_ecr_lifecycle_policy_document" "expire_non_master_non_latest_after_one_day" {
+  # Match tagged master, but expire them in 1000 years...
+  rule {
+    priority = 1
+    selection {
+      tag_status       = "tagged"
+      tag_pattern_list = ["master"]
+      count_type       = "sinceImagePushed"
+      count_unit       = "days"
+      count_number     = 365000
+    }
+  }
+  # otherwise match tagged latest, but expire them in 1000 years...
+  rule {
+    priority = 2
+    selection {
+      tag_status       = "tagged"
+      tag_pattern_list = ["latest"]
       count_type       = "sinceImagePushed"
       count_unit       = "days"
       count_number     = 365000
