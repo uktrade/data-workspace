f58bed804cc86a30dee68737be4bba3157775492
Isobel Daley <isobel.daley@6point6.co.uk>
2025-01-06 11:54:18 +0000

    feat: add security group rules for notebook endpoints in new sagemaker vpc

--- infra/modules/sagemaker_init/security/main.tf
+++ infra/modules/sagemaker_init/security/main.tf
@@ -14,8 +14,8 @@ resource "aws_security_group" "notebooks_endpoints" {
 }
 
 resource "aws_security_group_rule" "notebooks_endpoint_ingress_sagemaker" {
-  description       = "endpoint-ingress-from-datasets-vpc"
-  security_group_id = aws_security_group.notebooks_endpoints.id
+  description       = "endpoint-ingress-from-notebooks-vpc"
+  security_group_id = aws_security_group.sagemaker_endpoints.id
   cidr_blocks       = var.cidr_blocks
   type              = "ingress"
   from_port         = "0"
@@ -24,8 +24,8 @@ resource "aws_security_group_rule" "notebooks_endpoint_ingress_sagemaker" {
 }
 
 resource "aws_security_group_rule" "notebooks_endpoint_egress_sagemaker" {
-  description       = "endpoint-egress-from-datasets-vpc"
-  security_group_id = aws_security_group.notebooks_endpoints.id
+  description       = "endpoint-egress-from-notebooks-vpc"
+  security_group_id = aws_security_group.sagemaker_endpoints.id
   cidr_blocks       = var.cidr_blocks
   type              = "egress"
   from_port         = "0"
