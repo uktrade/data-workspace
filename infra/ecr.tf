9e62ed9192c3a7cc570520bac7b8dd1cb95e4a98
Isobel Daley <isobel.daley@6point6.co.uk>
2025-02-03 11:38:43 +0000

    fix: remove duplicated ECR policies

--- infra/ecr.tf
+++ infra/ecr.tf
@@ -13,11 +13,6 @@ resource "aws_ecr_lifecycle_policy" "user_provided_expire_untagged_after_one_day
   policy     = data.aws_ecr_lifecycle_policy_document.expire_untagged_after_one_day.json
 }
 
-resource "aws_ecr_lifecycle_policy" "user_provided_expire_untagged_after_one_day" {
-  repository = aws_ecr_repository.user_provided.name
-  policy     = data.aws_ecr_lifecycle_policy_document.expire_preview_and_untagged_after_one_day.json
-}
-
 resource "aws_ecr_repository" "admin" {
   name         = "${var.prefix}-admin"
   force_delete = false
