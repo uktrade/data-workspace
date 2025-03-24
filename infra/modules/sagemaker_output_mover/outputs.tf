97d61bf0186345e3c751feaf1dca6d028b7e4899
Aidan Russell <aidanrussell@users.noreply.github.com>
2025-03-24 16:58:28 +0000

    feat: add sagemaker output logic also for sagemaker errors as well as successful inferences (#288)

--- infra/modules/sagemaker_output_mover/outputs.tf
+++ infra/modules/sagemaker_output_mover/outputs.tf
@@ -1,3 +1,7 @@
 output "sns_success_topic_arn" {
   value = aws_sns_topic.async_sagemaker_success_topic.arn
 }
+
+output "sns_error_topic_arn" {
+  value = aws_sns_topic.async_sagemaker_error_topic.arn
+}
