c2fa42a73b61cd0707f1e1ec8f02910787fb3937
Tom Brooks <tombrooks248@gmail.com>
2025-04-09 16:26:41 +0100

    add aws_region and aws_caller_identity to 0201-data

--- infra/airflow_s3.tf
+++ infra/airflow_s3.tf
@@ -31,3 +31,49 @@ resource "aws_s3_bucket_policy" "airflow" {
   bucket = aws_s3_bucket.airflow[count.index].id
   policy = data.aws_iam_policy_document.airflow[count.index].json
 }
+
+data "aws_iam_policy_document" "airflow" {
+  count = var.airflow_on ? length(var.airflow_dag_processors) : 0
+  statement {
+    effect = "Deny"
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+    actions = [
+      "s3:*",
+    ]
+    resources = [
+      "arn:aws:s3:::${aws_s3_bucket.airflow[count.index].id}/*",
+    ]
+    condition {
+      test     = "Bool"
+      variable = "aws:SecureTransport"
+      values = [
+        "false"
+      ]
+    }
+  }
+
+  dynamic "statement" {
+    for_each = length(var.airflow_dag_processors[count.index].assume_roles) > 0 ? [1] : []
+    content {
+      effect = "Allow"
+      sid    = "Allow read access to data flow objects"
+
+      principals {
+        type        = "AWS"
+        identifiers = var.airflow_dag_processors[count.index].assume_roles
+      }
+
+      resources = [for prefix in var.s3_prefixes_for_external_role_copy : "arn:aws:s3:::${aws_s3_bucket.airflow[count.index].id}/${prefix}/*"]
+
+      actions = [
+        "s3:GetObject",
+        "s3:GetObjectTagging",
+        "s3:GetObjectVersion",
+        "s3:GetObjectVersionTagging"
+      ]
+    }
+  }
+}
