4e20aae508f6403f0d4f22e1587d7a91422ea84b
ince-dbt <148454056+ince-dbt@users.noreply.github.com>
2025-04-10 12:28:41 +0100

    Refactor core and datasets vpc (#334)
    ## What
    Refactor core and datasets vpc
    
    ## Why
    
    We are attempting to tidy up the structure of the data workspace
    terraform to follow the agreed format laid out in [this confluence
    page](https://uktrade.atlassian.net/wiki/spaces/DE/pages/4702437398/Data+Workspace+Terraform+Plan).
    
    <!---
    ## Links
    
    Links to give more detail or background, e.g. a ticket in JIRA or
    Trello, a release or commit in another repository. But all text above
    should be standalone: don't assume the reader can or will access any
    external links.
    
    e.g.
    See: [Description](https://example.com/)
    --->
    
    ## Checklist
    
    <!--- The commands `git commit --amend`, `git rebase -i` and `git push
    origin feat/my-change --force-with-lease` can be useful in acheiving the
    following -->
    
    * [ ] Each commit in the PR is
    [atomic](https://gds-way.cloudapps.digital/standards/source-code/working-with-git.html#atomic-commits).
    In many cases a single commit in the PR is appropriate.
    * [ ] Each commit has a [good
    message](https://gds-way.cloudapps.digital/standards/source-code/working-with-git.html#commit-messages),
    with a body and not just a title, ideally adhering to the [Conventional
    Commit Specification](https://www.conventionalcommits.org/en/v1.0.0/).
    * [ ] I have self-reviewed the PR - looking at the code changes and
    descriptions of all its commits.

--- infra/0201-core--vpc.tf
+++ infra/0201-core--vpc.tf
@@ -66,3 +66,14 @@ resource "aws_flow_log" "main" {
   vpc_id          = aws_vpc.main.id
   traffic_type    = "ALL"
 }
+
+resource "aws_vpc_endpoint" "main_s3" {
+  vpc_id            = aws_vpc.main.id
+  service_name      = "com.amazonaws.${data.aws_region.aws_region.name}.s3"
+  vpc_endpoint_type = "Gateway"
+}
+
+resource "aws_vpc_endpoint_route_table_association" "main_s3" {
+  vpc_endpoint_id = aws_vpc_endpoint.main_s3.id
+  route_table_id  = aws_route_table.private_with_egress.id
+}
