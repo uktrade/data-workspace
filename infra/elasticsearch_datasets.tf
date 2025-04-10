c88519c6131b78c74fa9ebd9d6ea744a88c8b278
Will Langdale <will.langdale@trade.gov.uk>
2025-04-10 12:17:00 +0100

    Added flag to IAM Service Linked Role for Elastic to allow deployment in different environments.

--- infra/elasticsearch_datasets.tf
+++ infra/elasticsearch_datasets.tf
@@ -2,5 +2,6 @@ resource "aws_iam_service_linked_role" "datasets_finder" {
   # This is a shared resource between all envs ... couldn't find a way to have
   # one per env as it automatically assigns the same name (and custom suffixes aren't
   # allowed for ES service linked roles).
+  count            = var.datasets_create_elastic_aws_service_linked_role ? 1 : 0
   aws_service_name = "es.amazonaws.com"
 }
