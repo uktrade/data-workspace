c23a9214bc0cbece39ec104ec1725edfe1ad0405
Michal Charemza <michal@charemza.name>
2025-04-11 04:53:57 +0100

    refactor: move core VPC endpoints to own file
    This moves the "core" VPC endpoints to its own numbered file.
    
    This is part of ongoing refactoring work to better structure the Terraform.
    
    Note that the "sagemaker" endpoints are not obviously/dfinitely part of "core",
    and indeed not all Data Workspace environments would have them.
    
    However:
    
    - They are in the "main" VPC, which is the "core" VPC.
    - And the frontend/admin application would also be part of "core", but would
      also conceivably have configuration/environment variables from other non-core
      components, so I think it's consistent to have "configurable" core-components
      depending on other components.

---
+++ infra/0202-core--vpc-endpoints.tf
@@ -0,0 +1,118 @@
+resource "aws_vpc_endpoint" "main_s3" {
+  vpc_id            = aws_vpc.main.id
+  service_name      = "com.amazonaws.${data.aws_region.aws_region.name}.s3"
+  vpc_endpoint_type = "Gateway"
+}
+
+resource "aws_vpc_endpoint_route_table_association" "main_s3" {
+  vpc_endpoint_id = aws_vpc_endpoint.main_s3.id
+  route_table_id  = aws_route_table.private_with_egress.id
+}
+
+resource "aws_vpc_endpoint" "ecr_dkr" {
+  vpc_id              = aws_vpc.main.id
+  service_name        = "com.amazonaws.${data.aws_region.aws_region.name}.ecr.dkr"
+  vpc_endpoint_type   = "Interface"
+  private_dns_enabled = true
+
+  security_group_ids = ["${aws_security_group.ecr_dkr.id}"]
+  subnet_ids         = ["${aws_subnet.private_with_egress.*.id[0]}"]
+
+  policy = data.aws_iam_policy_document.aws_vpc_endpoint_ecr.json
+
+  timeouts {}
+}
+
+resource "aws_vpc_endpoint" "ecr_api" {
+  vpc_id              = aws_vpc.main.id
+  service_name        = "com.amazonaws.${data.aws_region.aws_region.name}.ecr.api"
+  vpc_endpoint_type   = "Interface"
+  private_dns_enabled = true
+
+  security_group_ids = ["${aws_security_group.ecr_api.id}"]
+  subnet_ids         = ["${aws_subnet.private_with_egress.*.id[0]}"]
+
+  policy = data.aws_iam_policy_document.aws_vpc_endpoint_ecr.json
+
+  timeouts {}
+}
+
+data "aws_iam_policy_document" "aws_vpc_endpoint_ecr" {
+
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "aws:PrincipalAccount"
+      values = [
+        "${data.aws_caller_identity.aws_caller_identity.account_id}"
+      ]
+    }
+
+    actions = [
+      "*"
+    ]
+
+    resources = [
+      "*",
+    ]
+  }
+}
+
+resource "aws_vpc_endpoint" "sagemaker_runtime_endpoint_main" {
+  count              = var.sagemaker_on ? 1 : 0
+  vpc_id             = aws_vpc.main.id
+  service_name       = "com.amazonaws.eu-west-2.sagemaker.runtime"
+  vpc_endpoint_type  = "Interface"
+  subnet_ids         = aws_subnet.private_with_egress.*.id
+  security_group_ids = [aws_security_group.sagemaker_vpc_endpoints_main[0].id]
+  tags = {
+    Environment = var.prefix
+    Name        = "main-sagemaker-runtime-endpoint"
+  }
+  private_dns_enabled = true
+  policy              = data.aws_iam_policy_document.sagemaker_vpc_endpoint_policy[0].json
+}
+
+resource "aws_vpc_endpoint" "sagemaker_api_endpoint_main" {
+  count              = var.sagemaker_on ? 1 : 0
+  vpc_id             = aws_vpc.main.id
+  service_name       = "com.amazonaws.eu-west-2.sagemaker.api"
+  vpc_endpoint_type  = "Interface"
+  subnet_ids         = aws_subnet.private_with_egress.*.id
+  security_group_ids = [aws_security_group.sagemaker_vpc_endpoints_main[0].id]
+  tags = {
+    Environment = var.prefix
+    Name        = "main-sagemaker-api-endpoint"
+  }
+  private_dns_enabled = true
+  policy              = data.aws_iam_policy_document.sagemaker_vpc_endpoint_policy[0].json
+}
+
+data "aws_iam_policy_document" "sagemaker_vpc_endpoint_policy" {
+  # Prevents access to other AWS accounts through the VPC endpoint. There are policies on the
+  # roles themselves that restrict the actions and/or resources
+
+  count = var.sagemaker_on ? 1 : 0
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+    actions = [
+      "*",
+    ]
+    resources = [
+      "*",
+    ]
+    condition {
+      test     = "StringLike"
+      variable = "aws:PrincipalArn"
+      values   = ["arn:aws:iam::${data.aws_caller_identity.aws_caller_identity.account_id}:role/*"]
+    }
+  }
+}
