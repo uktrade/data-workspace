92cfe0a5567877d42cbb536ebd261e65c37ac1e0
Michal Charemza <michal@charemza.name>
2025-04-12 12:41:50 +0100

    refactor: inline container definition for Airflow DAG tasks
    This inlines ECS container definitions for Airflow DAG tasks
    
    This is part of ongoing work to tidy up the Terraform

--- infra/0408-airflow-team-tasks--ecs.tf
+++ infra/0408-airflow-team-tasks--ecs.tf
@@ -5,33 +5,67 @@ resource "aws_ecs_cluster" "airflow_dag_tasks" {
 resource "aws_ecs_task_definition" "airflow_dag_tasks" {
   count  = var.airflow_on ? 1 : 0
   family = "${var.prefix}-airflow-dag-tasks"
-  container_definitions = templatefile(
-    "${path.module}/airflow_dag_tasks_container_definitions.json", {
-      container_image = "${aws_ecr_repository.airflow.repository_url}:master"
-      container_name  = "airflow"
-      log_group       = "${aws_cloudwatch_log_group.airflow_webserver[count.index].name}"
-      log_region      = "${data.aws_region.aws_region.name}"
-
-      db_host     = "${aws_rds_cluster.airflow[count.index].endpoint}"
-      db_name     = "${aws_rds_cluster.airflow[count.index].database_name}"
-      db_password = "${random_string.aws_db_instance_airflow_password.result}"
-      db_port     = "${aws_rds_cluster.airflow[count.index].port}"
-      db_user     = "${aws_rds_cluster.airflow[count.index].master_username}"
-
-      datasets_db_host     = "${aws_rds_cluster.datasets.endpoint}"
-      datasets_db_name     = "${aws_rds_cluster.datasets.database_name}"
-      datasets_db_password = "${random_string.aws_rds_cluster_instance_datasets_password.result}"
-      datasets_db_port     = "${aws_rds_cluster.datasets.port}"
-      datasets_db_user     = "${var.datasets_rds_cluster_master_username}"
-
-      sentry_dsn         = "${var.sentry_notebooks_dsn}"
-      sentry_environment = "${var.sentry_environment}"
-
-      cloudwatch_log_group_arn = "${aws_cloudwatch_log_group.airflow_dag_tasks_airflow_logging[0].arn}"
-
-      dag_sync_github_key = "${var.dag_sync_github_key}"
+  container_definitions = jsonencode([
+    {
+      "environment" = [
+        {
+          "name"  = "DB_HOST",
+          "value" = aws_rds_cluster.airflow[count.index].endpoint,
+        },
+        {
+          "name"  = "DB_NAME",
+          "value" = aws_rds_cluster.airflow[count.index].database_name,
+        },
+        {
+          "name"  = "DB_PASSWORD",
+          "value" = random_string.aws_db_instance_airflow_password.result,
+        },
+        {
+          "name"  = "DB_PORT",
+          "value" = tostring(aws_rds_cluster.airflow[count.index].port)
+        },
+        {
+          "name"  = "DB_USER",
+          "value" = aws_rds_cluster.airflow[count.index].master_username
+        },
+        {
+          "name"  = "SENTRY_ENVIRONMENT",
+          "value" = var.sentry_environment
+        },
+        {
+          "name"  = "AIRFLOW__LOGGING__REMOTE_BASE_LOG_FOLDER",
+          "value" = "cloudwatch://${aws_cloudwatch_log_group.airflow_dag_tasks_airflow_logging[0].arn}"
+        },
+        {
+          # The key is already json-encoded, so to avoid it being double-json encoded, we decode it
+          # first. But to do this we need to wrap it in double quotes so it's valid JSON.
+          "name"  = "DAG_SYNC_GITHUB_KEY",
+          "value" = jsondecode("\"${local.airflow_dag_processor_container_vars[count.index].dag_sync_github_key}\"")
+        },
+      ],
+      "essential" = true,
+      "image"     = "${aws_ecr_repository.airflow.repository_url}:master",
+      "logConfiguration" = {
+        "logDriver" = "awslogs",
+        "options" = {
+          "awslogs-group"         = aws_cloudwatch_log_group.airflow_webserver[count.index].name,
+          "awslogs-region"        = data.aws_region.aws_region.name,
+          "awslogs-stream-prefix" = "airflow"
+        }
+      },
+      "networkMode" = "awsvpc",
+      "mountPoints" = [],
+      "name"        = "airflow",
+      "portMappings" = [
+        {
+          "containerPort" = 8080,
+          "hostPort"      = 8080,
+          "protocol"      = "tcp"
+        }
+      ]
     }
-  )
+  ])
+
   execution_role_arn       = aws_iam_role.airflow_webserver_execution[count.index].arn
   task_role_arn            = aws_iam_role.airflow_webserver_task[count.index].arn
   network_mode             = "awsvpc"
