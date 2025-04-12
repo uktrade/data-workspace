296941ef31993de41ee70d586b4f325702b1c9fb
Michal Charemza <michal@charemza.name>
2025-04-12 15:48:41 +0100

    refactor: inline visualisation container definition (#376)
    This inlines the container definition for (custom) visualisations.
    
    Note that because "sentry_dsn" is no longer passed to container definition JSON
    (because it was unused), the container definition JSON is no longer considered
    sensitive, and so terraform apply treats this as an actual change.
    
    This is part of the ongoing work to tidy up the Terraform

--- infra/0601-visualisations--ecs.tf
+++ infra/0601-visualisations--ecs.tf
@@ -1,26 +1,57 @@
 resource "aws_ecs_task_definition" "user_provided" {
   family = "${var.prefix}-user-provided"
-  container_definitions = templatefile(
-    "${path.module}/ecs_user_provided_container_definitions.json", {
-      container_name   = "${local.user_provided_container_name}"
-      container_cpu    = "${local.user_provided_container_cpu}"
-      container_memory = "${local.user_provided_container_memory}"
-      container_image  = "${aws_ecr_repository.user_provided.repository_url}"
-
-      log_group  = "${aws_cloudwatch_log_group.notebook.name}"
-      log_region = "${data.aws_region.aws_region.name}"
-
-      sentry_dsn = "${var.sentry_notebooks_dsn}"
-
-      metrics_container_image = "${aws_ecr_repository.metrics.repository_url}:master"
+  container_definitions = jsonencode([
+    {
+      "name"              = local.user_provided_container_name,
+      "image"             = aws_ecr_repository.user_provided.repository_url,
+      "memoryReservation" = local.user_provided_container_memory - 50,
+      "cpu"               = local.user_provided_container_cpu - 5,
+      "essential"         = true,
+      "ulimits" = [
+        {
+          "softLimit" = 4096,
+          "hardLimit" = 4096,
+          "name"      = "nofile"
+        }
+      ],
+      "logConfiguration" = {
+        "logDriver" = "awslogs",
+        "options" = {
+          "awslogs-group"         = aws_cloudwatch_log_group.notebook.name,
+          "awslogs-region"        = data.aws_region.aws_region.name,
+          "awslogs-stream-prefix" = local.user_provided_container_name
+        }
+      },
+      "environment" = []
+    },
+    {
+      "name"              = "metrics",
+      "image"             = "${aws_ecr_repository.metrics.repository_url}:master",
+      "memoryReservation" = 50,
+      "cpu"               = 5,
+      "essential"         = true,
+      "logConfiguration" = {
+        "logDriver" = "awslogs",
+        "options" = {
+          "awslogs-group"         = aws_cloudwatch_log_group.notebook.name,
+          "awslogs-region"        = data.aws_region.aws_region.name,
+          "awslogs-stream-prefix" = "metrics"
+        }
+      },
+      "environment" = [
+        {
+          "name"  = "PORT",
+          "value" = "8889"
+        },
+      ]
     }
-  )
+  ])
+
   execution_role_arn       = aws_iam_role.notebook_task_execution.arn
   network_mode             = "awsvpc"
   cpu                      = local.user_provided_container_cpu
   memory                   = local.user_provided_container_memory
   requires_compatibilities = ["FARGATE"]
-
 
   lifecycle {
     ignore_changes = [
