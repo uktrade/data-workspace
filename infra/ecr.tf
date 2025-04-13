0b5161a196823cb050cf923ee6765d5e9ce6114d
Michal Charemza <michal@charemza.name>
2025-04-13 14:39:30 +0100

    refactor: move datadog ecr to core (#409)
    This moves the definition for the datadog ECR repo to core. Although not yet,
    the rough plan is for it to be used in multiple services.
    
    This is part of the ongoing tidy up of the Terraform

--- infra/ecr.tf
+++ infra/ecr.tf
@@ -62,12 +62,3 @@ resource "aws_ecr_repository" "matchbox" {
   count = var.matchbox_on ? 1 : 0
   name  = "${var.prefix}-matchbox"
 }
-
-resource "aws_ecr_repository" "datadog" {
-  name = "${var.prefix}-datadog"
-}
-
-resource "aws_ecr_lifecycle_policy" "datadog_expire_untagged_after_one_day" {
-  repository = aws_ecr_repository.datadog.name
-  policy     = data.aws_ecr_lifecycle_policy_document.expire_untagged_after_one_day.json
-}
