b493f185501bf6360b5421d1f9178800643c87a3
Michal Charemza <michal@charemza.name>
2025-04-13 13:21:56 +0100

    refactor: avoid unnecessary templating (#407)
    This removes templating from the GitLab omnibus config, and uses Terraform
    variables directly
    
    Part of a wider tidy up of the Terraform

--- infra/0711-gitlab--ecs.tf
+++ infra/0711-gitlab--ecs.tf
@@ -62,27 +62,81 @@ resource "aws_ecs_task_definition" "gitlab" {
     {
       "environment" = [
         {
-          "name" = "GITLAB_OMNIBUS_CONFIG",
-          "value" = templatestring(
-            local.gitlab_omnibus_config, {
-              external_domain = "${var.gitlab_domain}"
-              db__host        = "${aws_rds_cluster.gitlab[count.index].endpoint}"
-              db__name        = "${aws_rds_cluster.gitlab[count.index].database_name}"
-              db__password    = "${random_string.aws_db_instance_gitlab_password.result}"
-              db__port        = "${aws_rds_cluster.gitlab[count.index].port}"
-              db__user        = "${aws_rds_cluster.gitlab[count.index].master_username}"
-
-              redis__host = "${aws_elasticache_cluster.gitlab_redis[count.index].cache_nodes.0.address}"
-              redis__port = "${aws_elasticache_cluster.gitlab_redis[count.index].cache_nodes.0.port}"
-
-              bucket__region = "${aws_s3_bucket.gitlab[count.index].region}"
-              bucket__domain = "${aws_s3_bucket.gitlab[count.index].bucket_regional_domain_name}"
-
-              sso__id     = "${var.gitlab_sso_id}"
-              sso__secret = "${var.gitlab_sso_secret}"
-              sso__domain = "${var.gitlab_sso_domain}"
+          "name"  = "GITLAB_OMNIBUS_CONFIG",
+          "value" = <<-EOT
+            external_url 'https://${var.gitlab_domain}'
+            nginx['listen_port'] = 80
+            nginx['listen_https'] = false
+            letsencrypt['enable'] = false
+            redis['enable'] = false
+            postgresql['enable'] = false
+            gitlab_rails['redis_host'] = '${aws_elasticache_cluster.gitlab_redis[count.index].cache_nodes.0.address}'
+            gitlab_rails['redis_port'] = ${aws_elasticache_cluster.gitlab_redis[count.index].cache_nodes.0.port}
+            gitlab_rails['db_adapter'] = 'postgresql'
+            gitlab_rails['db_encoding'] = 'utf8'
+            gitlab_rails['db_host'] = '${aws_rds_cluster.gitlab[count.index].endpoint}'
+            gitlab_rails['db_port'] = ${aws_rds_cluster.gitlab[count.index].port}
+            gitlab_rails['db_username'] = '${aws_rds_cluster.gitlab[count.index].master_username}'
+            gitlab_rails['db_password'] = '${random_string.aws_db_instance_gitlab_password.result}'
+            gitlab_rails['db_database'] = '${aws_rds_cluster.gitlab[count.index].database_name}'
+            gitlab_rails['uploads_object_store_enabled'] = true
+            gitlab_rails['uploads_object_store_remote_directory'] = 'uploads'
+            gitlab_rails['uploads_object_store_connection'] = {
+              'provider' => 'AWS',
+              'region' => '${aws_s3_bucket.gitlab[count.index].region}',
+              'host' => '${aws_s3_bucket.gitlab[count.index].bucket_regional_domain_name}',
+              'use_iam_profile' => true
             }
-          )
+            gitlab_rails['artifacts_enabled'] = true
+            gitlab_rails['artifacts_object_store_enabled'] = true;
+            gitlab_rails['artifacts_object_store_remote_directory'] = 'artifacts';
+            gitlab_rails['artifacts_object_store_connection'] = {
+              'provider' => 'AWS',
+              'region' => '${aws_s3_bucket.gitlab[count.index].region}',
+              'host' => '${aws_s3_bucket.gitlab[count.index].bucket_regional_domain_name}',
+              'use_iam_profile' => true
+            }
+            gitlab_rails['lfs_object_store_enabled'] = true
+            gitlab_rails['lfs_object_store_remote_directory'] = 'lfs-objects'
+            gitlab_rails['lfs_object_store_connection'] = {
+              'provider' => 'AWS',
+              'region' => 'eu-west-2',
+              'host' => '${aws_s3_bucket.gitlab[count.index].bucket_regional_domain_name}',
+              'use_iam_profile' => true
+            }
+            gitlab_rails['external_diffs_enabled'] = true
+            gitlab_rails['external_diffs_object_store_enabled'] = true
+            gitlab_rails['external_diffs_object_store_remote_directory'] = 'external-diffs'
+            gitlab_rails['external_diffs_object_store_connection'] = {
+              'provider' => 'AWS',
+              'region' => '${aws_s3_bucket.gitlab[count.index].region}',
+              'host' => '${aws_s3_bucket.gitlab[count.index].bucket_regional_domain_name}',
+              'use_iam_profile' => true
+            }
+            #https://gitlab.com/satorix/omniauth-oauth2-generic
+            gitlab_rails['omniauth_enabled'] = true
+            gitlab_rails['omniauth_allow_single_sign_on'] = ['oauth2_generic']
+            gitlab_rails['omniauth_auto_sign_in_with_provider'] = 'oauth2_generic'
+            gitlab_rails['omniauth_block_auto_created_users'] = false
+            gitlab_rails['omniauth_providers'] = [{
+              'name' => 'oauth2_generic',
+              'app_id' => '${var.gitlab_sso_id}',
+              'app_secret' => '${var.gitlab_sso_secret}',
+              'redirect_url' => 'https://${var.gitlab_domain}/auth/oauth2_generic/callback',
+              'args' => {
+                client_options: {
+                  'site' => 'https://${var.gitlab_sso_domain}',
+                  'authorize_url': '/o/authorize/',
+                  'token_url': '/o/token/',
+                  'user_info_url' => '/api/v1/user/me/'
+                },
+                user_response_structure: {
+                  root_path: [],
+                  id_path: ['user_id'],
+                }
+              }
+            }]
+          EOT
         },
         {
           "name"  = "BUCKET",
@@ -148,83 +202,6 @@ resource "aws_ecs_task_definition" "gitlab" {
       revision,
     ]
   }
-}
-
-locals {
-  gitlab_omnibus_config = <<-EOT
-    external_url 'https://$${external_domain}'
-    nginx['listen_port'] = 80
-    nginx['listen_https'] = false
-    letsencrypt['enable'] = false
-    redis['enable'] = false
-    postgresql['enable'] = false
-    gitlab_rails['redis_host'] = '$${redis__host}'
-    gitlab_rails['redis_port'] = $${redis__port}
-    gitlab_rails['db_adapter'] = 'postgresql'
-    gitlab_rails['db_encoding'] = 'utf8'
-    gitlab_rails['db_host'] = '$${db__host}'
-    gitlab_rails['db_port'] = $${db__port}
-    gitlab_rails['db_username'] = '$${db__user}'
-    gitlab_rails['db_password'] = '$${db__password}'
-    gitlab_rails['db_database'] = '$${db__name}'
-    gitlab_rails['uploads_object_store_enabled'] = true
-    gitlab_rails['uploads_object_store_remote_directory'] = 'uploads'
-    gitlab_rails['uploads_object_store_connection'] = {
-      'provider' => 'AWS',
-      'region' => '$${bucket__region}',
-      'host' => '$${bucket__domain}',
-      'use_iam_profile' => true
-    }
-    gitlab_rails['artifacts_enabled'] = true
-    gitlab_rails['artifacts_object_store_enabled'] = true;
-    gitlab_rails['artifacts_object_store_remote_directory'] = 'artifacts';
-    gitlab_rails['artifacts_object_store_connection'] = {
-      'provider' => 'AWS',
-      'region' => '$${bucket__region}',
-      'host' => '$${bucket__domain}',
-      'use_iam_profile' => true
-    }
-    gitlab_rails['lfs_object_store_enabled'] = true
-    gitlab_rails['lfs_object_store_remote_directory'] = 'lfs-objects'
-    gitlab_rails['lfs_object_store_connection'] = {
-      'provider' => 'AWS',
-      'region' => 'eu-west-2',
-      'host' => '$${bucket__domain}',
-      'use_iam_profile' => true
-    }
-    gitlab_rails['external_diffs_enabled'] = true
-    gitlab_rails['external_diffs_object_store_enabled'] = true
-    gitlab_rails['external_diffs_object_store_remote_directory'] = 'external-diffs'
-    gitlab_rails['external_diffs_object_store_connection'] = {
-      'provider' => 'AWS',
-      'region' => '$${bucket__region}',
-      'host' => '$${bucket__domain}',
-      'use_iam_profile' => true
-    }
-    #https://gitlab.com/satorix/omniauth-oauth2-generic
-    gitlab_rails['omniauth_enabled'] = true
-    gitlab_rails['omniauth_allow_single_sign_on'] = ['oauth2_generic']
-    gitlab_rails['omniauth_auto_sign_in_with_provider'] = 'oauth2_generic'
-    gitlab_rails['omniauth_block_auto_created_users'] = false
-    gitlab_rails['omniauth_providers'] = [{
-      'name' => 'oauth2_generic',
-      'app_id' => '$${sso__id}',
-      'app_secret' => '$${sso__secret}',
-      'redirect_url' => 'https://$${external_domain}/auth/oauth2_generic/callback',
-      'args' => {
-        client_options: {
-          'site' => 'https://$${sso__domain}',
-          'authorize_url': '/o/authorize/',
-          'token_url': '/o/token/',
-          'user_info_url' => '/api/v1/user/me/'
-        },
-        user_response_structure: {
-          root_path: [],
-          id_path: ['user_id'],
-        }
-      }
-    }]
-  EOT
 }
 
 resource "aws_iam_role" "gitlab_task_execution" {
