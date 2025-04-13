4e1d3a3deb15c14adecd851c64c2ba0a6858b46e
Michal Charemza <michal@charemza.name>
2025-04-13 09:27:50 +0100

    refactor: inline tools container definitions (#396)
    This inlines the container definitions for tools
    
    (I suspect) makes it touch easier to maintain, because the it removes a layer
    of indirection when making changes/looking what's hapenning.

--- infra/0603-tools--ecs.tf
+++ infra/0603-tools--ecs.tf
@@ -10,26 +10,88 @@ resource "aws_ecs_cluster" "notebooks" {
 resource "aws_ecs_task_definition" "tools" {
   count  = length(var.tools)
   family = "${var.prefix}-${var.tools[count.index].name}"
-  container_definitions = templatefile(
-    "${path.module}/ecs_notebooks_notebook_container_definitions.json", {
-      container_image = "${aws_ecr_repository.tools[count.index].repository_url}:master"
-      container_name  = "${local.notebook_container_name}"
-
-      log_group  = "${aws_cloudwatch_log_group.notebook.name}"
-      log_region = "${data.aws_region.aws_region.name}"
-
-      sentry_dsn         = "${var.sentry_notebooks_dsn}"
-      sentry_environment = "${var.sentry_environment}"
-
-      metrics_container_image = "${aws_ecr_repository.metrics.repository_url}:master"
-      s3sync_container_image  = "${aws_ecr_repository.s3sync.repository_url}:master"
-
-      cloudwatch_namespace = "${var.cloudwatch_namespace}"
-      cloudwatch_region    = "${var.cloudwatch_region}"
-
-      home_directory = "/home/coder"
+  container_definitions = jsonencode([
+    {
+      "name"      = local.notebook_container_name,
+      "image"     = "${aws_ecr_repository.tools[count.index].repository_url}:master",
+      "essential" = true,
+      "ulimits" = [{
+        "softLimit" = 4096,
+        "hardLimit" = 4096,
+        "name"      = "nofile"
+      }],
+      "logConfiguration" = {
+        "logDriver" = "awslogs",
+        "options" = {
+          "awslogs-group"         = aws_cloudwatch_log_group.notebook.name,
+          "awslogs-region"        = data.aws_region.aws_region.name,
+          "awslogs-stream-prefix" = local.notebook_container_name
+        }
+      },
+      "environment" = [{
+        "name"  = "SENTRY_DSN",
+        "value" = var.sentry_notebooks_dsn
+        }, {
+        "name"  = "SENTRY_ENVIRONMENT",
+        "value" = var.sentry_environment
+      }],
+      "mountPoints" = [{
+        "sourceVolume"  = "home_directory",
+        "containerPath" = "/home/coder"
+        }, {
+        "sourceVolume"  = "home_directory",
+        "containerPath" = "/home/dw-user"
+      }]
+    },
+    {
+      "name"      = "metrics",
+      "image"     = "${aws_ecr_repository.metrics.repository_url}:master",
+      "essential" = true,
+      "logConfiguration" = {
+        "logDriver" = "awslogs",
+        "options" = {
+          "awslogs-group"         = aws_cloudwatch_log_group.notebook.name,
+          "awslogs-region"        = data.aws_region.aws_region.name,
+          "awslogs-stream-prefix" = "metrics"
+        }
+      },
+      "environment" = [{
+        "name"  = "PORT",
+        "value" = "8889"
+      }]
+    },
+    {
+      "name"      = "s3sync",
+      "image"     = "${aws_ecr_repository.s3sync.repository_url}:master",
+      "essential" = true,
+      "logConfiguration" = {
+        "logDriver" = "awslogs",
+        "options" = {
+          "awslogs-group"         = aws_cloudwatch_log_group.notebook.name
+          "awslogs-region"        = data.aws_region.aws_region.name,
+          "awslogs-stream-prefix" = "s3sync"
+        }
+      },
+      "mountPoints" = [{
+        "sourceVolume"  = "home_directory",
+        "containerPath" = "/home/s3sync/data"
+      }],
+      "environment" = [{
+        "name"  = "SENTRY_DSN",
+        "value" = var.sentry_notebooks_dsn
+        }, {
+        "name"  = "SENTRY_ENVIRONMENT",
+        "value" = var.sentry_environment
+        }, {
+        "name"  = "CLOUDWATCH_MONITORING_NAMESPACE",
+        "value" = "${var.cloudwatch_namespace}/S3Sync"
+        }, {
+        "name"  = "CLOUDWATCH_MONITORING_REGION",
+        "value" = "${var.cloudwatch_region}"
+      }]
     }
-  )
+  ])
+
   execution_role_arn       = aws_iam_role.notebook_task_execution.arn
   network_mode             = "awsvpc"
   cpu                      = local.notebook_container_cpu
