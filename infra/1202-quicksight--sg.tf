56170f5b4b11861c16b2ee20eaa946aa7498c60d
Michal Charemza <michal@charemza.name>
2025-04-14 08:33:47 +0100

    security: remove unnecessary QuickSight security group rule
    This removes an unnecessary QuickSight security group rule, that theoretically
    would allow the datasets database to initiate connections to QuickSight. I
    strongly suspect this is already impossible/would acheive nothing already.
    
    It also adds a comment to explain why there is an unusual security group rule
    remaining that would not typically be needed for a client -> server connection.
    
    This is done to be as secure as possible - standard minimisation of
    privilege/swiss-cheese type of change. For the avoidance of doubt, I strongly
    suspect even without this change it is already impossible for the database to
    initial such a connection, certainly for a user to initiate such a connection,
    and also impossible if such a connection were possible to use the connection
    for anything.

--- infra/1202-quicksight--sg.tf
+++ infra/1202-quicksight--sg.tf
@@ -12,6 +12,15 @@ resource "aws_security_group" "quicksight" {
   }
 }
 
+# QuickSight makes the connection to the datasets database rather than vice versa, so we need the
+# usual egress rule on QuickSight's security group, and ingress rule on the database security
+# group. HOWEVER, strangely we also need an _ingress_ rule on QuickSight's security group. This is
+# because, from https://docs.aws.amazon.com/quicksight/latest/user/vpc-inbound-rules.html
+#
+# > The security group attached to the QuickSight network interface behaves differently than most
+# > security groups, because it isn't stateful. [...] To make it work [...] make sure to add an
+# > inbound rule that explicitly authorizes the return traffic from the database host.
+
 resource "aws_security_group_rule" "quicksight_ingress_all_from_datasets_db" {
   description = "ingress-all-from-datasets-db"
 
@@ -43,18 +52,6 @@ resource "aws_security_group_rule" "datasets_db_ingress_all_from_quicksight" {
   source_security_group_id = aws_security_group.quicksight.id
 
   type      = "ingress"
-  from_port = "0"
-  to_port   = "65535"
-  protocol  = "tcp"
-}
-
-resource "aws_security_group_rule" "datasets_db_egress_all_to_quicksight" {
-  description = "egress-all-to-quicksight"
-
-  security_group_id        = aws_security_group.datasets.id
-  source_security_group_id = aws_security_group.quicksight.id
-
-  type      = "egress"
   from_port = "0"
   to_port   = "65535"
   protocol  = "tcp"
