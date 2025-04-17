e21226a7b628c9ed718d531a97d6d8abbb9d97a4
Michal Charemza <michal@charemza.name>
2025-04-17 14:56:59 +0100

    security: remove GitLab ECS service access to ECR
    This removes GitLab's security-group level access to ECR. Note that this
    removes access from the service itself (i.e. from inside the running Docker
    container), and not the EC2 instance that pulls the Docker image from ECR.
    
    This is a belt-and-braces/swiss cheese change - doesn't close an actual known
    security vulnerability for several reasons, but since GitLab shouldn't be able
    to communicate with ECR, removing the security group rules that allows it to.

--- infra/1010-gitlab--ecr.tf
+++ infra/1010-gitlab--ecr.tf
@@ -6,12 +6,3 @@ resource "aws_ecr_lifecycle_policy" "gitlab_expire_untagged_after_one_day" {
   repository = aws_ecr_repository.gitlab.name
   policy     = data.aws_ecr_lifecycle_policy_document.expire_untagged_after_one_day.json
 }
-
-module "gitlab_outgoing_https_ecr_api" {
-  count  = var.gitlab_on ? 1 : 0
-  source = "./modules/security_group_client_server_connections"
-
-  client_security_groups = [aws_security_group.gitlab_service[count.index]]
-  server_security_groups = [aws_security_group.ecr_api]
-  ports                  = [443]
-}
