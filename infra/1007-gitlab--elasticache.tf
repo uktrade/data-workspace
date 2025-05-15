015fb24f65461d766ee65a7e420687b723e2e330
Michal Charemza <michal@charemza.name>
2025-05-15 17:06:45 +0100

    fix: correct parameter group for Redis 7 (#455)
    This corrects the parameter group for Redis 7
    
    Otherwise terraform will not apply properly.

--- infra/1007-gitlab--elasticache.tf
+++ infra/1007-gitlab--elasticache.tf
@@ -4,7 +4,7 @@ resource "aws_elasticache_cluster" "gitlab_redis" {
   engine               = "redis"
   node_type            = "cache.t2.micro"
   num_cache_nodes      = 1
-  parameter_group_name = "default.redis5.0"
+  parameter_group_name = "default.redis7"
   engine_version       = "7.1"
   port                 = 6379
   subnet_group_name    = aws_elasticache_subnet_group.gitlab[count.index].name
