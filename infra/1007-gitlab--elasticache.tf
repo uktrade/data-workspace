412a8eba4b39acccb260ef7160ee5f1c00a49684
Michal Charemza <michal@charemza.name>
2025-05-15 14:37:00 +0100

    infra: increase the version of GitLab Redis
    This increases the from of GitLab Redis from 5.0.6 to 7.1, and sets changes to
    be applied immediately.
    
    This is because GitLab 16 seems to require a version of Redis >= 6, and applies
    immediately because that is what we want/expect, to not have to wait until a
    maintenance window.

--- infra/1007-gitlab--elasticache.tf
+++ infra/1007-gitlab--elasticache.tf
@@ -5,10 +5,11 @@ resource "aws_elasticache_cluster" "gitlab_redis" {
   node_type            = "cache.t2.micro"
   num_cache_nodes      = 1
   parameter_group_name = "default.redis5.0"
-  engine_version       = "5.0.6"
+  engine_version       = "7.1"
   port                 = 6379
   subnet_group_name    = aws_elasticache_subnet_group.gitlab[count.index].name
   security_group_ids   = ["${aws_security_group.gitlab_redis[count.index].id}"]
+  apply_immediately    = true
 }
 
 resource "aws_elasticache_subnet_group" "gitlab" {
