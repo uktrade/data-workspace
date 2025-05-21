3e46496dbaa5a42a69971f0beecf0ef7b38db7dc
Michal Charemza <michal@charemza.name>
2025-05-21 16:58:00 +0100

    infra: GitLab ECR lifecycle policy
    This changes the GitLab ECR lifecycle policy to expire all but the most
    recently pushed image.
    
    This is the best we can come up with given the constraints of the ECR lifecycle
    system. For example, we would base things on pull or push date, or make sure to
    keep images that are running, but these are not possible.

--- infra/1010-gitlab--ecr.tf
+++ infra/1010-gitlab--ecr.tf
@@ -2,7 +2,28 @@ resource "aws_ecr_repository" "gitlab" {
   name = "${var.prefix}-gitlab"
 }
 
-resource "aws_ecr_lifecycle_policy" "gitlab_expire_untagged_after_one_day" {
+resource "aws_ecr_lifecycle_policy" "gitlab" {
   repository = aws_ecr_repository.gitlab.name
-  policy     = data.aws_ecr_lifecycle_policy_document.expire_untagged_after_one_day.json
+  policy     = data.aws_ecr_lifecycle_policy_document.gitlab.json
+}
+
+data "aws_ecr_lifecycle_policy_document" "gitlab" {
+  rule {
+    priority = 1
+    selection {
+      tag_status       = "tagged"
+      tag_pattern_list = ["*"]
+      count_type       = "imageCountMoreThan"
+      count_number     = 1
+    }
+  }
+  rule {
+    priority = 2
+    selection {
+      tag_status   = "untagged"
+      count_type   = "sinceImagePushed"
+      count_unit   = "days"
+      count_number = 1
+    }
+  }
 }
